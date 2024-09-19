622afadf5fb7bf6793d5ee13a84b4da1b5d29cff
Raghavendra Kakarla <quic_rkakarla@quicinc.com>
2024-09-19 09:17:21 -0700

    power-utils: Add power mode monitor API and notifier program
    This is the initial stub definitions of the pm-client APIs and pm-notify
    program.
    
    Change-Id: I9168a6f1c44d3135e4e7b823a56fb9bdfc9765ba
    Signed-off-by: Eric Smith <quic_ericsmit@quicinc.com>
    Co-authored-by: Raghavendra Kakarla <quic_rkakarla@quicinc.com>
    (cherry picked from commit 15629b8d71aac77145e8ba22cb2cf1dffd9ddc2d)

---
+++ power-utils/src/pm-monitor.c
@@ -0,0 +1,18 @@
+/* Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+* SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
+
+#include "pm_client_lib.h"
+
+/* Stub definition until implementation is added */
+int pm_register(const char *name, struct pm_ops_s *ops, void *ctxt, pm_client_t *hdl)
+{
+    return 0;
+}
+
+
+/* Stub definition until implementation is added */
+int pm_deregister(pm_client_t hdl)
+{
+    return 0;
+}
---
+++ power-utils/src/pm-notify.c
@@ -0,0 +1,8 @@
+/* Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+* SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
