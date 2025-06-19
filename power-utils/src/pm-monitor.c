cf404a1e9ecb13ede58cca3407589c25a5dcd053
Guodong Hu <quic_guodhu@quicinc.com>
2025-06-18 17:49:11 -0700

    power-utils: Set client thread name to "pm_monitor"
    Change-Id: I8233587deca3ca7628c21942372b8ac5b6ac41ec
    Signed-off-by: Guodong Hu <quic_guodhu@quicinc.com>

--- power-utils/src/pm-monitor.c
+++ power-utils/src/pm-monitor.c
@@ -36,6 +36,8 @@ static void *monitor_pm_notifications(void *hdl)
 	FD_ZERO(&read_fds);
 	FD_SET(pm_hdl->listen_fd, &read_fds);
 
+	pthread_setname_np(pthread_self(), "pm_monitor");
+
 	while (1) {
 		fprintf(stderr, SD_INFO "waiting for pm notifications on %s\n", pm_hdl->pm_sock->sun_path);
 		temp_fds = read_fds;
