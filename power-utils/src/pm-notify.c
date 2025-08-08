31099438bf7c872f4fed7c72dce24317b7e50091
Ajender Reddy Mallikarjuna <ajenderr@qti.qualcomm.com>
2025-08-07 21:52:41 -0700

    power-utils: add impose_v2 API to support passing custom LPM modes
    Change-Id: Ia7993ec69d76e23831a68568b340573402170a10
    Signed-off-by: Ajender Reddy Mallikarjuna <ajenderr@qti.qualcomm.com>

--- power-utils/src/pm-notify.c
+++ power-utils/src/pm-notify.c
@@ -16,30 +16,49 @@
 #include "pm_server_lib.h"
 
 void print_usage() {
-	fprintf(stderr, SD_INFO "Usage: pm-notify name <pm command> [impose level]\n"
-			"\t\timpose <impose level>");
+	fprintf(stderr, SD_INFO "Usage: pm-notify name...\n"
+			"\t\t%s\n"
+			"\t\t%s\n"
+			"\t\t%s <impose level>\n"
+			"\t\t%s <impose level> <lpm mode>\n",
+			PM_ENTER_CMD,
+			PM_EXIT_CMD,
+			IMPOSE_CMD,
+			IMPOSE_V2_CMD
+		);
+	exit(0 - EINVAL);
 }
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2) {
+	int ret;
+	if (argc < 3) {
 		print_usage();
-		return -1;
 	}
 
 	char client_name[UNIX_PATH_MAX];
 	char pm_cmd[PM_CMD_LEN];
 	int impose_level = -1;
+	int lpm_mode = -1;
 
 	strlcpy(client_name, argv[1], UNIX_PATH_MAX);
 	strlcpy(pm_cmd, argv[2], PM_CMD_LEN);
 
 	if (!strcmp(pm_cmd, IMPOSE_CMD)) {
-		if (argc != 3) {
+		if (argc < 4) {
 			fprintf(stderr, SD_ERR "impose command requires <impose level> argument\n\n");
 			print_usage();
 		}
 		impose_level = atoi(argv[3]); // TODO change to strtol so we can detect errors
+	}
+	else if (!strcmp(pm_cmd, IMPOSE_V2_CMD)) {
+		if (argc < 5) {
+			fprintf(stderr, SD_ERR "impose command requires <impose level> argument\n\n");
+			print_usage();
+		}
+		impose_level = atoi(argv[3]); // TODO change to strtol so we can detect errors
+		lpm_mode = atoi(argv[4]);
+		return pm_send_notif_v2(client_name, pm_cmd, impose_level, lpm_mode);
 	}
 
 	return pm_send_notif(client_name, pm_cmd, impose_level);
--- power-utils/src/pm-server.c
+++ power-utils/src/pm-server.c
@@ -9,6 +9,9 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <time.h>
 
 #include <systemd/sd-daemon.h>
 #include "bootkpi/logging.h"
@@ -16,6 +19,38 @@
 #include "pm_client_lib.h"
 #include "pm-internal.h"
 #include "pm_server_lib.h"
+
+static int trace_fd = -1;
+
+void trace_log_init(void) {
+	trace_fd = open("/sys/kernel/debug/tracing/instances/suspend_resume/trace_marker", O_WRONLY);
+	if (trace_fd < 0) {
+		fprintf(stderr, "trace_marker is not available\n");
+	}
+}
+
+void trace_log_write(const char *format, ...) {
+	if (trace_fd < 0) {
+		fprintf(stderr, "trace_log_write: trace_fd not initialized\n");
+		return;
+	}
+
+	char buffer[1024];
+	va_list args;
+
+	va_start(args, format);
+	vsnprintf(buffer, sizeof(buffer), format, args);
+	va_end(args);
+
+	write(trace_fd, buffer, strlen(buffer));
+}
+
+void trace_log_close(void) {
+	if (trace_fd >= 0) {
+		close(trace_fd);
+		trace_fd = -1;
+	}
+}
 
 /* pm_cmd is either suspend or resume
  *
@@ -38,30 +73,17 @@ static int set_suspend_pm_data(struct pm_event *pm_data) {
 	return 0;
 }
 
-int pm_send_notif(char *name, char *pm_cmd, int mode)
+static int send_event(const char *name, const struct pm_event *pm_data)
 {
 	int fd_type = SOCK_STREAM | SOCK_CLOEXEC;
 	int pm_notify_fd;
+	double elapsed_sec;
 	char buf[MAX_BUF_LEN] = {'\0'};
-	char buffer[256];
 	char pm_ops[PM_MAX_BUF_LEN] = {'\0'};
 	char suspend_resume[PM_MAX_BUF_LEN] = {'\0'};
 	static struct sockaddr_un pm_notify;
-	struct pm_event pm_data;
-
-	if (!strcmp(pm_cmd, IMPOSE_CMD)) {
-		pm_data.mode = mode; // mode is used as the impose level
-	}
-	else if (!strcmp(pm_cmd, PM_ENTER_CMD) || !strcmp(pm_cmd, PM_EXIT_CMD)) {
-		set_suspend_pm_data(&pm_data);
-	}
-	else {
-		// pm_cmd is invalid
-		fprintf(stderr, SD_ERR "Error: invalid pm_cmd %s\n", pm_cmd);
-		return -ENODEV;
-	}
-
-	strlcpy(pm_data.cmd, pm_cmd, PM_CMD_LEN);
+	struct timespec start, end;
+	double elapsed;
 
 	pm_notify_fd = socket(PF_UNIX, fd_type, 0);
 	if (pm_notify_fd < 0) {
@@ -82,38 +104,94 @@ int pm_send_notif(char *name, char *pm_cmd, int mode)
 		return -ENODEV;
 	}
 
-	fprintf(stderr, SD_INFO "sending pm_data.cmd %s, pm_data.mode %d\n", pm_data.cmd, pm_data.mode);
-
-	if (pm_data.mode == PM_MODE_DS)
+	if (pm_data->mode == PM_MODE_DS)
 		strlcpy(pm_ops,"deepsleep" , PM_MAX_BUF_LEN);
-	else if (pm_data.mode == PM_MODE_S2R)
+	else if (pm_data->mode == PM_MODE_S2R)
 		strlcpy(pm_ops,"str" , PM_MAX_BUF_LEN);
 
-	if (!strcmp(pm_cmd, PM_ENTER_CMD))
+	if (!strcmp(pm_data->cmd, PM_ENTER_CMD))
 		strlcpy(suspend_resume, "suspend" , PM_MAX_BUF_LEN);
-	else if (!strcmp(pm_cmd, PM_EXIT_CMD))
+	else if (!strcmp(pm_data->cmd, PM_EXIT_CMD))
 		strlcpy(suspend_resume, "resume" , PM_MAX_BUF_LEN);
 
-	pm_log_line("start :%s :%s :%s", name, pm_ops , suspend_resume);
-	send(pm_notify_fd, (unsigned char *)&pm_data, sizeof(pm_data), 0);
-	fprintf(stderr, SD_INFO "sent pm_data.cmd %s, pm_data.mode %d\n", pm_data.cmd, pm_data.mode);
+	fprintf(stderr, SD_INFO "%s: sending pm_data.cmd %s, pm_data.mode %s, pm_data.lpm_mode %d\n", name, suspend_resume, pm_ops, pm_data->lpm_mode);
+	trace_log_init();
+
+	clock_gettime(CLOCK_BOOTTIME, &start);
+	send(pm_notify_fd, (const unsigned char *)pm_data, sizeof(*pm_data), 0);
+	fprintf(stderr, SD_INFO "%s: sent pm_data.cmd %s, pm_data.mode %s, pm_data.lpm_mode %d\n", name, suspend_resume, pm_ops, pm_data->lpm_mode);
 	recv(pm_notify_fd, (unsigned char *)buf, MAX_BUF_LEN, 0);
 	close(pm_notify_fd);
 
-	fprintf(stderr, SD_INFO "Received %s from client\n", buf);
+	fprintf(stderr, SD_INFO "Received %s from client-%s\n", buf, name);
+
+	clock_gettime(CLOCK_BOOTTIME, &end);
+	elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
 
 	if (!strcmp(ACK_RESPONSE, buf)) {
-		fprintf(stderr, SD_INFO "Received ACK from client\n");
-		pm_log_line("end :%s :%s :%s", name, pm_ops , suspend_resume);
+		fprintf(stderr, SD_INFO "Received ACK from client %s\n", name);
+
+		if  (!strcmp(pm_data->cmd, PM_ENTER_CMD) || !strcmp(pm_data->cmd, PM_EXIT_CMD)) {
+			pm_log_line("total_time %.6f seconds:%s :%s :%s", elapsed_sec, name, pm_ops , suspend_resume);
+			trace_log_write("%s :%s :%s :%.6f seconds", name, pm_ops , suspend_resume, elapsed);
+		}
+		trace_log_close();
 		return 0;
 	}
 
 	if (!strcmp(NACK_RESPONSE, buf)) {
-		fprintf(stderr, SD_CRIT "Received NACK from client\n");
+		fprintf(stderr, SD_CRIT "Received NACK from client %s\n", name);
+		trace_log_close();
 		return -1;
 	}
 
-	fprintf(stderr, SD_CRIT "Received unknown response from client\n");
+	fprintf(stderr, SD_CRIT "Received unknown response from client %s\n", name);
+	trace_log_close();
 
 	return -EAGAIN;
+}
+
+int pm_send_notif(char *name, char *pm_cmd, int mode)
+{
+	struct pm_event pm_data = {0};
+
+	if (!strcmp(pm_cmd, IMPOSE_CMD)) {
+		pm_data.mode = mode; // mode is used as the impose level
+	}
+	else if (!strcmp(pm_cmd, PM_ENTER_CMD) || !strcmp(pm_cmd, PM_EXIT_CMD)) {
+		set_suspend_pm_data(&pm_data);
+	}
+	else {
+		// pm_cmd is invalid
+		fprintf(stderr, SD_ERR "Error: invalid pm_cmd %s\n", pm_cmd);
+		return -ENODEV;
+	}
+
+	strlcpy(pm_data.cmd, pm_cmd, PM_CMD_LEN);
+
+	return send_event(name, &pm_data);
+}
+
+int pm_send_notif_v2(char *name, char *pm_cmd, int mode, int lpm_mode)
+{
+	struct pm_event pm_data = {0};
+
+	if (!strcmp(pm_cmd, IMPOSE_CMD) || !strcmp(pm_cmd, IMPOSE_V2_CMD)) {
+		pm_data.mode = mode; // mode is used as the impose level
+		if (!strcmp(pm_cmd, IMPOSE_V2_CMD)) {
+			pm_data.lpm_mode = lpm_mode;
+		}
+	}
+	else if (!strcmp(pm_cmd, PM_ENTER_CMD) || !strcmp(pm_cmd, PM_EXIT_CMD)) {
+		set_suspend_pm_data(&pm_data);
+	}
+	else {
+		// pm_cmd is invalid
+		fprintf(stderr, SD_ERR "Error: invalid pm_cmd %s\n", pm_cmd);
+		return -ENODEV;
+	}
+
+	strlcpy(pm_data.cmd, pm_cmd, PM_CMD_LEN);
+
+	return send_event(name, &pm_data);
 }
