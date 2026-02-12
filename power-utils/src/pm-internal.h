652c76e234a629aadd7bfe471a6e91b1f3393a6d
RAMESHWAR PRASAD SAHU <ramesahu@qti.qualcomm.com>
2026-02-12 16:33:25 +0530

    power-utils: Addressing incompatibility between GPU libraries and the power-utils code base in RB
    The GPU libraries are built using the mainline power-utils code base, and
    the same binaries are used across all RBs. This leads to power-utils not
    calling pm_cancel due to incompatibility issues.
    To resolve this, the following changelist from the mainline has been ported into RB:
    
    Commit Id 8734597344967825043f2d6c8ec57daa8bf597f9: pm-server: Removed uninitialized variable
    Commit Id 31099438bf7c872f4fed7c72dce24317b7e50091: power-utils: add impose_v2 API to support passing custom LPM modes
    Commit Id d9ae46f32e9d9f56089627fc43e64f844d338531: Merge "power-utils: Add KPI'S for STR and DSQB."
    Commit Id 1d74a742995afee818731b1d90cef9d62b269790: power-utils: Add KPI'S for STR and DSQB.
    
    Change-Id: I7947991c0ea34112e955e5c7669ceb3b48c7aff1
    Signed-off-by: Rameshwar Prasad Sahu <ramesahu@qti.qualcomm.com>

--- power-utils/src/pm-internal.h
+++ power-utils/src/pm-internal.h
@@ -23,7 +23,10 @@
 #define UNIX_PATH_MAX 108
 #define PM_CMD_LEN 50
 
+#define PM_MAX_BUF_LEN		20
+
 #define IMPOSE_CMD "impose"
+#define IMPOSE_V2_CMD "impose_v2"
 #define PM_ENTER_CMD "pm-enter"
 #define PM_EXIT_CMD "pm-exit"
 #define ACK_RESPONSE "success"
@@ -43,6 +46,7 @@ struct _pm_client_s {
 struct pm_event {
 	char cmd[PM_CMD_LEN];
 	int mode; // can be used for suspend mode or impose level
+	int lpm_mode; // lpm mode used for impose_v2
 };
 
 int get_socket_path(const char *client_name, char *socket_path);
